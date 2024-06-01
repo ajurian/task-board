dc603e8c8ba77cac4df6268c641dca90296b766c
AdolfUrian <adolfjamesurian@gmail.com>
2024-06-01 17:18:47 +0800

    Fixed focus accesibility bug

--- src/app/board/[uniqueName]/components/TaskBoard/hooks/useContentEditable.tsx
+++ src/app/board/[uniqueName]/components/TaskBoard/hooks/useContentEditable.tsx
@@ -91,7 +91,8 @@ export default function useContentEditable<T extends HTMLElement>({
         (e) => {
             if (
                 focusedElement === null ||
-                e.currentTarget.contains(e.relatedTarget)
+                (e.currentTarget.contains(e.relatedTarget) &&
+                    e.currentTarget !== e.relatedTarget)
             ) {
                 return;
             }
